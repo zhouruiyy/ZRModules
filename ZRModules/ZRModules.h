7b6278b1de89beaa237edbf98ec0698c06cd1851
zhourui09 <zhourui09@baidu.com>
2020-04-20 15:16:53 +0800

    Initial Commit

---
+++ ZRModules/ZRModules.h
@@ -0,0 +1,19 @@
+//
+//  ZRModules.h
+//  ZRModules
+//
+//  Created by Zhou,Rui(ART) on 2020/4/20.
+//  Copyright © 2020 Zhou,Rui(ART). All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ZRModules.
+FOUNDATION_EXPORT double ZRModulesVersionNumber;
+
+//! Project version string for ZRModules.
+FOUNDATION_EXPORT const unsigned char ZRModulesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZRModules/PublicHeader.h>
+
+
